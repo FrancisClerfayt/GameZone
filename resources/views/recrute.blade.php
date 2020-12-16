db00ad2a45d30bc9ad44b60a05afb6d8d6855092
claire dupuich <1097457668@qq.com>
2020-12-16 22:00:46 +0100

    recrute

--- resources/views/recrute.blade.php
+++ resources/views/recrute.blade.php
@@ -4,14 +4,14 @@
 
 @section('content')
 <h2 class="titre_recrute">Vous souhaitez nous rejoindre</h2>
-<p class="text_recrute">Notre équipe recrute de temps à autre les postes suivants: personnel de direction, personnel technique, personnel de vente, personnel de service. Si vous êtes intéressé, veuillez nous appeler ou nous envoyer un email</p>
+<p class="text_recrute">Notre équipe recrute de temps à autre les postes suivants: personnel de direction, personnel technique, personnel de vente, personnel de service. Si vous êtes intéressé, veuillez nous appeler ou nous envoyer un email.</p>
 <div class="contact_recrute">
-<img src="" alt="" class="icon_recrute">
+<img src="../public/images/icon_phone.png" alt="" class="icon_recrute">
 <p class="short_text">0606006006</p>
 </div>
 
 <div class="contact_recrute">
-<img src="public/images/" alt="" class="icon_recrute">
+<img src="../public/images/icon_email.png" alt="" class="icon_recrute">
 <p class="short_text">gamezone@gamil.com</p>
 </div>
 
