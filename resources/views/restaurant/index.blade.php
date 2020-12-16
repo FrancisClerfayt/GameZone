76b76f96cf022160fc01d00771e3918e3b31eec5
claire dupuich <1097457668@qq.com>
2020-12-16 14:38:01 +0100

    css of page restaurent

--- resources/views/restaurant/index.blade.php
+++ resources/views/restaurant/index.blade.php
@@ -2,20 +2,20 @@
 
 @extends('layouts.app')    
 
-<!-- //添加商品后最初的显示页面，它的css可以直接放到本文件的html里，也可以尝试到public-> css->style.css 里 -->
+<!-- //restaurant的显示页面，它的css可以直接放到本文件的html里，也可以尝试到public-> css->style.css 里 -->
 
 @section('content')
-   <h2 class="first_titre">Nos restaurants</h2>
+   <h2 class="first_titre_r">Nos restaurants</h2>
     <div class="container_restaurant">
        
                  @foreach ($restaurants as $restaurant)
                     <div class="items_restaurant">
                        
-                         <img src=" {{asset($restaurant.image)}} " class="img_r_left" style="height: 200px">
+                         <img src=" {{asset($restaurant->image)}} " class="img_r_left" >
                          <div class="restaurant_right">  
                             <h3>{{$restaurant->name}}</h3>
                             <p class="restaurant_text">{{$restaurant->description}}</p>
-                            <a href=" {{ route('restaurant.show', ['restaurant', $restaurant]) }} " class="restaurant_button">En savoir plus</a>
+                            <a href=" {{ route('Restaurant.show', ['restaurant', $restaurant]) }} " class="restaurant_button">En savoir plus</a>
 
                          </div>
                     </div>
