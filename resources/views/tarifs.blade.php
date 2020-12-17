7246c297d5efcf44c346332d5796b4ae714b4a19
claire dupuich <1097457668@qq.com>
2020-12-17 15:38:48 +0100

    tarifs finish

--- resources/views/tarifs.blade.php
+++ resources/views/tarifs.blade.php
@@ -8,7 +8,7 @@
 
 <p class="tarif_age">L’entrée pour les enfants de -2 ans est gratuite !</p>
 
-<div class="contenaire_tarifs">
+<!-- <div class="contenaire_tarifs">
 <div class="tarifs_items">
     <img src="{{ URL::asset('images/ticket_children_2to8yr.png') }}" alt="" class="img_tarifs">
     <p class="text_intems text_intems1">Tarifs enfants de 2 a 8 ans <br>12,50 € </p>
@@ -18,9 +18,20 @@
     <p class="text_intems text_intems2">Tarifs enfants de plus de 8 ans<br>13,50 €</p>
 </div>
 <div class="tarifs_items">
-    <img src="{{ URL::asset('images/ticket_adults_over18yr.jpg') }}" alt="" class="img_tarifs">
+    <img src="{{ URL::asset('images/ticket_adults_over18yr.png') }}" alt="" class="img_tarifs">
     <p class="text_intems text_intems3">Tarifs adulte (+18) <br>15 € </p>
 </div>
+</div> -->
+
+<div class="contenaire_tarifs">
+
+
+    <img src="{{ URL::asset('images/ticket_children_2to8yr.png') }}" alt="" class="img_tarifs">
+ 
+    <img src="{{ URL::asset('images/ticket_children_over8yr.jpg') }}" alt="" class="img_tarifs">
+   
+    <img src="{{ URL::asset('images/ticket_adults_over18yr.png') }}" alt="" class="img_tarifs">
+   
 </div>
 
 <h3 class="tarifs_text_tiket">Vous pourrez faire l’achat des billets à votre arrivée au parc</h3>
