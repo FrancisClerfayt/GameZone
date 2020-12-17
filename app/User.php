bb3fe38792d4408e182b76ff75bc55cf596de45a
FrancisClerfayt <francis.clerfayt@gmail.com>
2020-12-17 15:21:02 +0100

    inscription with avatar done

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'last_name','first_name','user_name','city', 'email', 'password', 'avatar'
     ];
 
     /**
