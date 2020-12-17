b79b68816121a21db90ac855a99b216546f2632b
FrancisClerfayt <francis.clerfayt@gmail.com>
2020-12-17 11:09:24 +0100

    fixed login & added admin token

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('email_verified_at')->default(now());
             $table->string('password');
             $table->rememberToken();
             $table->string('first_name');
