1a328af9a753c12401fd6567c8db881f34143f93
FrancisClerfayt <francis.clerfayt@gmail.com>
2020-12-17 01:43:09 +0100

    added html, css and js for map

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,16 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
+<<<<<<< HEAD
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/home', 'HomeController@index')->name('loggedHome');
 Route::get('/map', 'HomeController@map')->name('map');
 Route::get('/legalNotice','LegalNoticeController@index')->name('legal_notice');
 Route::get('/terms_of_sales','TermsOfSales@index')->name('terms_of_sales');
+=======
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/map', 'HomeController@map')->name('map');
+>>>>>>> added html, css and js for map
 
 Route::resource('Restaurant', 'RestaurantController');
 Route::resource('Attraction', 'AttractionController');
