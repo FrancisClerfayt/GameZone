82afd9f76ef7fdb9f9f8b223347a652aa9a94f9c
FrancisClerfayt <francis.clerfayt@gmail.com>
2020-12-21 21:25:03 +0100

    fixed game errors

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -8,7 +8,7 @@
 			<div>
 				<h2>Tetris</h2>
 				<p>Score : <span id="score">0</span></p>
-				<p>Lignes : <span id="nbLines">0</span></p>
+				<p>Lignes : <span id="lines">0</span></p>
 				<p>Level : <span id="level">0</span></p>
 				<canvas class="next" id="next"></canvas>
 			</div>
@@ -16,8 +16,8 @@
 		</div>
 	</div>
 </div>
-<script src="{{ asset('js/constant.js') }}"></script>
-<script src="{{ asset('js/piece.js') }}"></script>
-<script src="{{ asset('js/board.js') }}"></script>
-<script src="{{ asset('js/game.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/constant.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/piece.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/board.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/game.js') }}"></script>
 @endsection
