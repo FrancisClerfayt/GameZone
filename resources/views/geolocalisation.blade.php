ba6282f498626399560ad4eaa9b3f46ec16decc1
Jose59300 <j.freirelopes26@gmail.com>
2020-12-21 11:04:56 +0100

    geolocalisation page done

--- resources/views/geolocalisation.blade.php
+++ resources/views/geolocalisation.blade.php
@@ -4,28 +4,31 @@
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
 <link rel="stylesheet" href="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.css" />
 <link rel="stylesheet" href="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.css" />
-<h2>Trouvez votre chemin jusqu'à nous !</h2>
-<section>
-  <div class="">
-  <p>Nous nous situons non loin de la frontiére Belge, dans les Hauts-De-France, plus précisément à Anzin !</p>
-  <h4>Adresse</h4>
-  <address class="">
-    80 avenue Roland Moreno, 59410 Anzin
-  </address>
+<h2 class="text-center">Trouvez votre chemin jusqu'à nous !</h2>
+<section class="container geolocalisation">
+  <div class="row">
+    <div class="adress text-center col-md-6">
+      <p class="text_geolocalisation">Nous nous situons non loin de la frontiére Belge, dans les Hauts-De-France, plus précisément à Anzin !</p>
+      <address class="">
+        <h3>Adresse</h3>
+        80 avenue Roland Moreno, 59410 Anzin
+      </address>
+    </div>
+    <div class="col-md-6" id="carte"></div>
+
   </div>
-  <div id="carte"></div>
 </section>
 <div class="">
-  <h4><i class="fas fa-train"></i>Se rendre au parc en train</h4>
-  <p>La gare la plus proche se situe à Valenciennes, à quelques minutes de nous ! Retrouvez toutes les informations sur <a href="https://www.sncf.com/fr"></a></p>
+  <h3 class="text-center title_geolocalisation"><i class="fas fa-train"></i>  Se rendre au parc en train</h3>
+  <p class="text-center text_geolocalisation">La gare la plus proche se situe à Valenciennes, à quelques minutes de nous ! Retrouvez toutes les informations sur <a href="https://www.sncf.com/fr">sncf.com</a></p>
 </div>
 <div class="">
-  <h4><i class="fab fa-avianex"></i>Se rendre au parc en avion</h4>
-  <p>Les aéroports les plus proches se situe a Lesquin, Charleroi et Bruxelles</p>
+  <h3 class="text-center title_geolocalisation"><i class="fab fa-avianex"></i>  Se rendre au parc en avion</h3>
+  <p class="text-center text_geolocalisation">Les aéroports les plus proches se situe a Lesquin, Charleroi et Bruxelles</p>
 </div>
 <div class="">
-  <h4><i class="fas fa-car"></i>Venez jusqu'à nous avec notre partenaire Blabacar</h4>
-  <p>Rendez-vous sur le site <a href="https://www.blablacar.fr/"></a> et profité d'une réduction de 10% avec le code "GAMEZONE" et venez vous amusez à prix réduit !</p>
+  <h3 class="text-center title_geolocalisation"><i class="fas fa-car"></i>  Venez jusqu'à nous avec notre partenaire Blabacar</h3>
+  <p class="text-center text_geolocalisation">Rendez-vous sur le site <a href="https://www.blablacar.fr/">Blabacar.fr</a> et venez vous amusez à prix réduit !</p>
 </div>
 
 
